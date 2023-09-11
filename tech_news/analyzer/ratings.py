cbc8f67e6d4c43b908a1df3498fc7d422bcd03a7
Amanda Zanata <amandazanata46@gmail.com>
2023-09-11 20:52:21 -0300

    Requisito 6

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,17 @@
+from tech_news.database import db
+
 # Requisito 10
+
+
 def top_5_categories():
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    create = [
+        {"$group": {"_id": "$category", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1, "_id": 1}},
+        {"$limit": 5},
+    ]
+
+    result_list = db.news.aggregate(create)
+
+    categories = [category["_id"] for category in result_list]
+
+    return categories
