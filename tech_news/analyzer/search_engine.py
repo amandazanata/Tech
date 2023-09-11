089f646f5a5e646a1b46fa76c95586674adb058c
Amanda Zanata <amandazanata46@gmail.com>
2023-09-11 20:46:11 -0300

    Requisitos 6 7 8 9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,16 +1,33 @@
+from datetime import datetime
+from tech_news.database import search_news
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    return [
+        (news["title"], news["url"])
+        for news in search_news({"title": {"$regex": title, "$options": "i"}})
+    ]
 
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    try:
+        new = datetime.strptime(date, "%Y-%m-%d").strftime("%d/%m/%Y")
+        result = search_news({"timestamp": {"$regex": new}})
+        search_results = []
+        for result in result:
+            search_results.append((result["title"], result["url"]))
+        return search_results
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    return [
+        (news["title"], news["url"])
+        for news in search_news(
+            {"category": {"$regex": category, "$options": "i"}}
+        )
+    ]
